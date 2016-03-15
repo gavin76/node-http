414c7d813f729f9653e7ed5f1ab2687e03cd548b
Gavin Chan <freshgav@gmail.com>
2016-03-15 22:45:30 +0800

    completed server-2 code

--- server-2.js
+++ server-2.js
@@ -15,7 +15,25 @@ var server = http.createServer(function(req, res) {
 		var filePath = path.resolve('./public' + fileUrl);
 		var fileExt = path.extname(filePath);
 		if (fileExt === '.html') {
-			
+			fs.exists(filePath, function(exists) {
+				if (!exists) {
+					res.writeHead(404, { 'Content-Type': 'text/html' });
+					res.end('<html><body><h1>Error 404: ' + fileUrl + ' not found</h1></body></html>');
+					return;
+				}
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				fs.createReadStream(filePath).pipe(res);
+			});
+		} else {
+			res.writeHead(404, {'Content-Type': 'text/html' });
+			res.end('<html><body><h1>Error 404: ' + fileUrl + ' not a HTML file</h1></body></html>');
 		}
+	} else {
+		res.writeHead(404, { 'Content-Type': 'text/html' });
+		res.end('<html><body><h1>Error 404: ' + req.method + ' not supported</h1></body></html>');
 	}
 })
+
+server.listen(port, hostname, function() {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
